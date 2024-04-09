28fd386d9dd95142c665b6c23b10e6e5d6f0e463
912-Coroiu-Adela <adela.coroiu@stud.ubbcluj.ro>
2024-04-09 20:05:32 +0300

    Partially done

---
+++ app/app/CreateUserAccount.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace app
+{
+    public partial class CreateUserAccount : ContentPage
+    {
+        public CreateUserAccount()
+        {
+            InitializeComponent();
+        }
+
+        private void UserButton_Clicked(object sender, EventArgs e)
+        {
+            // Switch to User mode
+        }
+
+        private void ArtistButton_Clicked(object sender, EventArgs e)
+        {
+            // Switch to Artist mode
+        }
+
+        private void CreateButton_Clicked(object sender, EventArgs e)
+        {
+            // Perform create action
+        }
+    }
+}
--- app/app/MauiProgram.cs
+++ app/app/MauiProgram.cs
@@ -1,25 +1,26 @@
-﻿using Microsoft.Extensions.Logging;
+﻿    using Microsoft.Extensions.Logging;
 
-namespace app
-{
-    public static class MauiProgram
+    namespace app
     {
-        public static MauiApp CreateMauiApp()
+        public static class MauiProgram
         {
-            var builder = MauiApp.CreateBuilder();
-            builder
-                .UseMauiApp<App>()
-                .ConfigureFonts(fonts =>
-                {
-                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-                });
+            public static MauiApp CreateMauiApp()
+            {
+                var builder = MauiApp.CreateBuilder();
+                builder
+                    .UseMauiApp<App>()
+                    .ConfigureFonts(fonts =>
+                    {
+                        fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                        fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    });
 
-#if DEBUG
-    		builder.Logging.AddDebug();
-#endif
+    #if DEBUG
+    		    builder.Logging.AddDebug();
+    #endif
+                
 
-            return builder.Build();
+                return builder.Build();
+            }
         }
     }
-}
