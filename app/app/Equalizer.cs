ac36862aecab0bfc8b9152e77a0d99e680cb10a7
912-Cernautan-Teofan <115460421+912-Cernautan-Teofan@users.noreply.github.com>
2024-04-09 22:18:54 +0300

    Equilzer class done and ready for review

---
+++ app/app/Equalizer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace app
+{
+    internal class Equalizer
+    {
+        // Properties
+        public Dictionary<string, string> BalanceParameters { get; set; }
+        public bool Active { get; private set; }
+        public int BassValue { get; private set; }
+        // Constructor
+        public Equalizer()
+        {
+            BalanceParameters = new Dictionary<string, string>();
+            Active = false;
+            BassValue = 0;
+        }
+
+        // BalanceParameters changer
+        public void ChangeBalanceParameters(Dictionary<string, string> newParameters)
+        {
+            BalanceParameters = newParameters;
+        }
+
+        public void AdjustBass(int bassLevel)
+        {
+            BassValue = bassLevel;
+        }
+        // Activator
+        public void Activate()
+        {
+            Active = true;
+        }
+
+        // Deactivator
+        public void Deactivate()
+        {
+            Active = false;
+        }
+    }
+}
