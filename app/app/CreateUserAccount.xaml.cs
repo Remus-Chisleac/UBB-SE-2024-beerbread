cf2f21dee8d9271567310e94978e2ef2cc61d707
912-Coroiu-Adela <adela.coroiu@stud.ubbcluj.ro>
2024-04-10 12:09:33 +0300

    Finished Create User Account Interface

--- app/app/CreateUserAccount.xaml.cs
+++ app/app/CreateUserAccount.xaml.cs
@@ -26,6 +26,39 @@ namespace app
         private void CreateButton_Clicked(object sender, EventArgs e)
         {
             // Perform create action
+            string username = UserEntryUsername.Text;
+            string password = UserEntryPassword.Text;
+            string email = UserEntryEmail.Text;
+
+            if (username.Length < 6)
+            {
+                DisplayAlert("Username Error", "Must be at least 6 characters", "OK");
+                return;
+            }
+
+            if (password.Length < 8)
+            {
+                DisplayAlert("Password Error", "Must be at least 8 characters", "OK");
+                return;
+            }
+
+            if (!email.EndsWith("@gmail.com") && !email.EndsWith("@yahoo.com"))
+            {
+                DisplayAlert("Email Error", "Should end with @gmail.com or @yahoo.com", "OK");
+                return;
+            }
+
+            //pop up on screen saying "Account created successfully"
+            DisplayAlert("Alert", "Account created successfully", "OK");
+
+        }
+
+        private void LoginButton_Clicked(object sender, EventArgs e)
+        {
+
+            //pop up on screen saying "Login page not implemented yet"
+            DisplayAlert("Alert", "Login page not implemented yet", "OK");
+
         }
     }
 }
