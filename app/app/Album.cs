4d9fbc6026076a304ba7569df594c5674e944d0b
912-Contiu-Mario <mario.contiu@stud.ubbcluj.ro>
2024-04-12 14:34:29 +0300

    Implemented Album class

---
+++ app/app/Album.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace app
+{
+    internal class Album: GenericSongRepo
+    {
+
+        public bool isPublic { get; set; }
+        public Album(int id, string name, bool isPublic): base(id, name)
+        {
+            this.isPublic = isPublic;
+        }
+
+        public bool EmptyPlaylist()
+        {
+            try {
+                this.songs.Clear();
+                return true;
+            }
+            catch(Exception exception)
+            {
+                return false;
+            }
+        }
+
+    }
+}
