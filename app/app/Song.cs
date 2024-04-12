c99688a4c0ee1f2be5baaaf95e39c5b81bec38ec
912-Contiu-Mario <mario.contiu@stud.ubbcluj.ro>
2024-04-12 13:44:58 +0300

    Implemented song class

---
+++ app/app/Song.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace app
+{
+    internal class Song
+    {
+        private string name { get; set; }
+        private string artist { get; set; }
+        private int likes { get; set; }
+        private string album { get; set; }
+
+        private int id { get; set; }
+        private int duration { get; set; }
+
+        private int timePlayed { get; set; }
+    }
+}
