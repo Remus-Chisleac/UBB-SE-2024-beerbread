d61731176280b9df31af86135b9beadf0a58813e
912-Contiu-Mario <mario.contiu@stud.ubbcluj.ro>
2024-04-12 13:49:29 +0300

    forgot constructor

--- app/app/Song.cs
+++ app/app/Song.cs
@@ -7,15 +7,30 @@ using System.Threading.Tasks;
 namespace app
 {
     internal class Song
+
+
     {
-        private string name { get; set; }
-        private string artist { get; set; }
-        private int likes { get; set; }
-        private string album { get; set; }
 
         private int id { get; set; }
+        private string name { get; set; }
+        private string artist { get; set; }
+        private string album { get; set; }
+
+        private int likes { get; set; }
+
         private int duration { get; set; }
 
         private int timePlayed { get; set; }
+
+        public Song(int id, string name, string artist, string album, int duration)
+        {
+            this.id = id;
+            this.name = name;
+            this.artist = artist;
+            this.album = album;
+            this.duration = duration;
+            this.likes = 0;
+            this.timePlayed = 0;
+        }
     }
 }
