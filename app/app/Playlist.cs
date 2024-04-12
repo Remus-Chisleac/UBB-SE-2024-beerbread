1c017fac99dbaebd0c429a1543eaa4b7eb0ad8ca
912-Contiu-Mario <mario.contiu@stud.ubbcluj.ro>
2024-04-12 14:38:35 +0300

    Implemented Playlist class

---
+++ app/app/Playlist.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace app
+{
+    internal class Playlist: GenericSongRepo
+    {
+
+        public bool isPublic { get; set; }
+        public Playlist(int id, string name, bool isPublic): base(id, name)
+        {
+            this.isPublic = isPublic;
+        }
+
+        public bool EmptyPlaylist()
+        {
+            try {
+                this.songs.Clear();
+                return true;
+            }
+            catch(Exception exception)
+            {
+                return false;
+            }
+        }
+
+    }
+}
