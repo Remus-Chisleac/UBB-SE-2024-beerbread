aa28ac6cb2e55ac7949c30f0cbfc60ab10a755bf
Tomkode <thomasd810@gmail.com>
2024-04-13 13:28:29 +0300

    Implemented the User and Playlist classes

---
+++ app/app/Playlist.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace app
+{
+    
+    internal class Playlist : GenericSongRepo
+    {
+        public bool isPublic { get; set; }
+        public Playlist(int id, string name) : base(id, name)
+        {
+            isPublic = false; // By default, a playlist is private
+        }
+        public bool emptyPlaylist()
+        {
+            return songs.Count == 0;
+        }
+    }
+    
+    
+}
