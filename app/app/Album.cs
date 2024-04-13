64a35d8fb8e1927d7cc1eb102ba37134a3e171e3
912-Contiu-Mario <mario.contiu@stud.ubbcluj.ro>
2024-04-13 17:13:33 +0300

    Implemented Album class and moved song number from Album to GenericSongRepo

--- app/app/Album.cs
+++ app/app/Album.cs
@@ -14,8 +14,5 @@ namespace app
         {
             this.description = description;
         }
-
-       
-
     }
 }
---
+++ app/app/GenericSongRepo.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace app
+{
+    internal class GenericSongRepo
+    {
+        public Dictionary<int, Song> songs { get; set; }
+        public int id { get; set; }
+        public string name { get; set; }
+
+        public GenericSongRepo(int id, string name)
+        {
+            this.id = id;
+            this.name = name;
+            songs = new Dictionary<int, Song>();
+        }
+
+
+
+        public bool AddSong(Song songToAdd)
+        {
+            try
+            {
+                songs.Add(songToAdd.id, songToAdd);
+            }
+            catch (Exception exception)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public bool RemoveSong(Song songToRemove)
+        {
+            return songs.Remove(songToRemove.id);
+
+        }
+
+        public int GetSongNumbert()
+        {
+            return songs.Count;
+        }
+
+    }
+}
