1218f611939ee42a0ad77c1cd737c6342fa1443d
SoloScout <chisleacrrr@gmail.com>
2024-04-14 18:20:43 +0300

    fix playlist files

--- app/app/Playlist.cs
+++ app/app/Playlist.cs
@@ -6,11 +6,11 @@ using System.Threading.Tasks;
 
 namespace app
 {
-    
+
     internal class Playlist : GenericSongRepo
     {
         public bool isPublic { get; set; }
-        public Playlist(int id, string name, bool isPublic = false) : base(id, name)
+        public Playlist(int id, Guid owner, string name, bool isPublic = false) : base(owner, id, name)
         {
             this.isPublic = isPublic; // By default, a playlist is private
         }
@@ -19,6 +19,6 @@ namespace app
             return songs.Count == 0;
         }
     }
-    
-    
+
+
 }
