60a5ee68e446e282cc6e51f31e827f250aeecc31
SoloScout <chisleacrrr@gmail.com>
2024-04-14 19:54:31 +0300

    login and playlists tab

--- app/app/CreateAccountDefault.xaml.cs
+++ app/app/CreateAccountDefault.xaml.cs
@@ -2,23 +2,31 @@ namespace app;
 
 public partial class CreateAccountDefault : ContentPage
 {
-	public CreateAccountDefault()
-	{
-		InitializeComponent();
-	}
-
-	private void UserButton_Clicked(object sender, EventArgs e)
-	{
-        // Switch to User mode
-		CreateUserAccount createUserAccount = new CreateUserAccount();
-		Navigation.PushAsync(createUserAccount);
+    public CreateAccountDefault()
+    {
+        InitializeComponent();
     }
 
-	private void ArtistButton_Clicked(object sender, EventArgs e)
-	{
+    private void UserButton_Clicked(object sender, EventArgs e)
+    {
+        // Switch to User mode
+        CreateUserAccount createUserAccount = new CreateUserAccount();
+        Navigation.PushAsync(createUserAccount);
+    }
+
+    private void ArtistButton_Clicked(object sender, EventArgs e)
+    {
         // Switch to Artist mode
-		CreateArtistAccount createArtistAccount = new CreateArtistAccount();
-		Navigation.PushAsync(createArtistAccount);
+        CreateArtistAccount createArtistAccount = new CreateArtistAccount();
+        Navigation.PushAsync(createArtistAccount);
+    }
+
+    private void LoginButton_Clicked(object sender, EventArgs e)
+    {
+
+        src.LogIn logIn = new src.LogIn();
+        Navigation.PushAsync(logIn);
+
     }
 
 
