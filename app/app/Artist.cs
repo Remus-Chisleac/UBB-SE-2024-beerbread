8806745c7d8f698a592304e6bc320df1c823bd15
Tomkode <thomasd810@gmail.com>
2024-04-14 23:00:26 +0300

    Implemented Artist

---
+++ app/app/Artist.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace app
+{
+    internal class Artist : User
+    {
+        public Dictionary<int, Album> albums { get; set; }
+        public Artist(string email, string username, string salt, string hashPassword) : base(email, username, salt, hashPassword)
+        {
+            albums = new Dictionary<int, Album>();
+        }
+    }
+}
