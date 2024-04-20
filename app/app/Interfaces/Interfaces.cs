a1847478296d3bd7fa7e7e7be09f89b27b53e78f
papadia <bianca.nistor55@gmail.com>
2024-04-20 17:00:09 +0300

    did interfaces for album and genericsongrepo

--- app/app/Interfaces/Interfaces.cs
+++ app/app/Interfaces/Interfaces.cs
@@ -6,7 +6,39 @@ using System.Threading.Tasks;
 
 namespace app.Interfaces
 {
-    internal class Class1
+    public interface IAccountService
     {
+        bool CreateUserAccount(string email, string username, string password);
+        bool Authenticate(string email, string password);
     }
+
+    public interface ISqlAccountService
+    {
+        bool AddAccount(Account account);
+        bool AddUserAccount(Account account);
+        Account GetAccount(string email);
+        string GetAccountHashedPassword(string email);
+    }
+    public interface IAlbum
+    {
+        string Description { get; set; }
+        int Owner { get; }
+        int Id { get; }
+        string Name { get; }
+        bool AddSong(int songId);
+        bool RemoveSong(int songId);
+        int GetSongsNumber();
+    }
+
+    public interface IGenericSongRepo
+    {
+        List<int> Songs { get; set; }
+        int Id { get; }
+        string Name { get; }
+        int Owner { get; }
+        bool AddSong(int songId);
+        bool RemoveSong(int songId);
+        int GetSongsNumber();
+    }
+
 }
