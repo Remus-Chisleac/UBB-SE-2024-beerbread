32977479c9d9080b0dd72c86419140ced3b65ec0
Mindrila Alex <mihail.mindrila@stud.ubbcluj.ro>
2024-04-22 03:59:40 +0300

    Implemented song repository
    Test it!!!

---
+++ app/app/Data/Repositories/SqlSongRepository.cs
@@ -0,0 +1,32 @@
+﻿namespace app.src.SqlDataStorageAndRetrival
+{
+    using System.Collections.Generic;
+    using app.Data.SqlCommandHandlers;
+
+    public interface ISqlSongRepository
+    {
+        List<int> GetAllSongIds();
+
+        List<Song> GetSongsWithIds(List<int> songIds);
+    }
+
+    public class SqlSongRepository : ISqlSongRepository
+    {
+        private ISqlSongTableCommandExecutor sqlSongTableCommandExecutor;
+
+        public SqlSongRepository()
+        {
+            this.sqlSongTableCommandExecutor = new SqlSongTableCommandExecutor();
+        }
+
+        public List<int> GetAllSongIds()
+        {
+            return this.sqlSongTableCommandExecutor.GetAllSongIdsInDatabase();
+        }
+
+        public List<Song> GetSongsWithIds(List<int> songIds)
+        {
+            return this.sqlSongTableCommandExecutor.GetSongsWithIds(songIds);
+        }
+    }
+}
---
+++ app/app/Data/SqlCommandHandlers/SqlSongTableCommandExecutor.cs
@@ -0,0 +1,87 @@
+﻿namespace app.Data.SqlCommandHandlers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Data;
+    using System.Diagnostics;
+    using System.Text;
+    using app.Data.ServerHandlers;
+    using Microsoft.Data.SqlClient;
+
+    public interface ISqlSongTableCommandExecutor
+    {
+        List<int> GetAllSongIdsInDatabase();
+
+        List<Song> GetSongsWithIds(List<int> songIds);
+    }
+
+    internal class SqlSongTableCommandExecutor : SqlCommandExecutor, ISqlSongTableCommandExecutor
+    {
+        private readonly SqlConnection currentSqlConnection;
+
+        public SqlSongTableCommandExecutor()
+        {
+            this.currentSqlConnection = StaticSqlConnectionGenerator.GetConnection();
+        }
+
+        public List<int> GetAllSongIdsInDatabase()
+        {
+
+            List<int> allSongIds = new List<int>();
+            try
+            {
+                this.currentSqlConnection.Open();
+                SqlCommand command = new ("SELECT id FROM songs", this.currentSqlConnection);
+                SqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    allSongIds.Add(reader.GetInt32("id"));
+                }
+
+                reader.Close();
+                this.currentSqlConnection.Close();
+            }
+            catch (Exception ex)
+            {
+                Trace.WriteLine(ex.Message);
+            }
+
+            return allSongIds;
+        }
+
+        public List<Song> GetSongsWithIds(List<int> songIds)
+        {
+            List<Song> songs = new List<Song>();
+            try
+            {
+                this.currentSqlConnection.Open();
+                string stringIds = string.Empty;
+                foreach (int id in songIds)
+                {
+                    stringIds += id + ",";
+                }
+
+                SqlCommand command = new ("SELECT * FROM Songs WHERE id in (" + stringIds.Substring(0, stringIds.Length - 1) + ")", this.currentSqlConnection);
+                SqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    songs.Add(new Song(
+                               reader.GetInt32("id"),
+                               reader.GetString("songName"),
+                               reader.GetString("artistName"),
+                               reader.GetString("urlSong"),
+                               reader.GetString("urlImage")));
+                }
+
+                reader.Close();
+                this.currentSqlConnection.Close();
+            }
+            catch (Exception ex)
+            {
+                Trace.WriteLine(ex.Message);
+            }
+
+            return songs;
+        }
+    }
+}
