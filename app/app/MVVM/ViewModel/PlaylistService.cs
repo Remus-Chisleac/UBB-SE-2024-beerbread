170c46fec0754f98bfb9033ec7865258d5e38095
915-Mihalachi-Miruna <mirunamihalache54@gmail.com>
2024-04-23 21:33:39 +0300

    structured the playlist page and added palylist service + got rid of some warnings

---
+++ app/app/MVVM/ViewModel/PlaylistService.cs
@@ -0,0 +1,30 @@
+﻿using app.MVVM.Model.Data.Repositories;
+using app.MVVM.Model.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace app.MVVM.ViewModel
+{
+    internal class PlaylistService
+    {
+        private ISqlPlaylistRepository playlistRepository;
+
+        public PlaylistService()
+        {
+            this.playlistRepository = new SqlPlaylistRepository();
+        }
+
+        public List<IPlaylist> GetUserPlaylists(Guid userId)
+        {
+            return this.playlistRepository.GetUserPlaylists(userId);
+        }
+
+        internal void AddPlaylist(IPlaylist playlist, Guid id)
+        {
+            this.playlistRepository.AddPlaylist(playlist, id);
+        }
+    }
+}
